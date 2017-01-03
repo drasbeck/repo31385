11fc3462c99c1975344e41ed94d95288361dd242
Max Drasbeck <max@drasbeck.dk>
2017-01-03 17:04:18 +0100

    ex3

--- ex4/plot_ex4_32.m
+++ ex4/plot_ex4_32.m
@@ -1,9 +1,6 @@
-data = importdata('data_log_ex4_32.dat');
+clear all; clc; close all;
 
-figure;
-plot(data(:,5),data(:,6));
-
-data = importdata('data_log_ex4_32_ccw.dat');
+data = importdata('data_log.dat');
 
 figure;
 plot(data(:,5),data(:,6));
